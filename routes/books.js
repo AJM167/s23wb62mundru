759d3b9700bd1c492202ab0b372135396fd969a1
AJM167 <123146230+AJM167@users.noreply.github.com>
2023-04-05 14:38:13 -0500

    updated books

--- routes/books.js
+++ routes/books.js
@@ -3,7 +3,7 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function (req, res, next) {
-  res.render('index', { title: 'Asha' });
+    res.render('books', { title: 'Search Results books' });
 });
 
 module.exports = router;
